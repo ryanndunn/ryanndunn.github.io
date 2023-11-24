1b36cb9a616a3a44e25576c2e0e826175847e13e
Ryan Dunn <ryan.d@rollwithduckpin.com>
2023-11-24 16:45:19 -0500

    bg img fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,9 @@ import Seo from '../components/seo'
 const AboutPage = () => {
   return (
     <Layout pageContainerClass="page--about">
+      <div className="page--content">
         <h1>About Me</h1>
+      </div>
     </Layout>
   )
 }
