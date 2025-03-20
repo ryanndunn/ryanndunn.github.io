8108df1ca104d83a7213da25f6963a936d463e65
ryanndunn <rdunn32789@gmail.com>
2025-03-19 22:18:11 -0400

    added new resume

--- src/components/nav.js
+++ src/components/nav.js
@@ -13,7 +13,7 @@ const Nav = () => {
             <Link to="/about">About</Link>
         </li>
         <li>
-            <a href={'/ryan-dunn-resume-public.pdf'} target="_blank" rel="noreferrer">Resume</a>
+            <a href={'/resume-rd.pdf'} target="_blank" rel="noreferrer">Resume</a>
         </li>
       </ul>
     </nav>
